950465161e6cd549b2889fc51d46fbe93d36ebdf
ImOrion <mateou771@gmail.com>
2022-10-26 17:53:58 -0500

    Tarea re pff

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,8 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if (matchFunc(startEl)) resultSet.push(startEl);
   
+  for(let i = 0; i < startEl.children.length; i++){
+    let results = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+    resultSet = [...resultSet, ...results]
+  }
+  return resultSet
 };
+
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
@@ -18,7 +25,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0]==="#")return "id";
+  if(selector[0] === ".")return "class"
+  if(selector.includes(".")) return "tag.class";
+  return "tag"
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,15 +39,23 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
+
   if (selectorType === "id") { 
-   
+    matchFunction=elementoHTML=>`#${elementoHTML.id}` === selector
   } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
+    matchFunction=function(elementoHTML){
+      return elementoHTML.classList.contains(selector.substring(1));
+    };
   } else if (selectorType === "tag") {
-    
+    matchFunction=function(elementoHTML){
+    return elementoHTML.tagName.toLowerCase()===selector.toLowerCase();}
+  } else if (selectorType === "tag.class") {
+    matchFunction=function(elementoHTML){
+      const [tag,clase] = selector.split(".");
+
+      return (elementoHTML.tagName.toLowerCase()===tag.toLowerCase())&&(elementoHTML.classList.contains(clase))
   }
+}
   return matchFunction;
 };
 
