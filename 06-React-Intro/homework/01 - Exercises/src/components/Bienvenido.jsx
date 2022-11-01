7ca219216fa1dda067005a27ed71564dcdc8939b
ImOrion <mateou771@gmail.com>
2022-11-01 09:20:21 -0500

    Commit diosmio

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Botones from "./Botones.jsx"
 
-const studentName = "";
+const studentName = "Ani";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+	// el código de tu componente acá
+	return (
+		<div>
+			<h1>Hola mundo</h1>
+			<h3>{studentName}</h3>
+
+			<ul>
+				{techSkills.map(skill => (
+					<li>{skill}</li>
+				))}
+			</ul>
+
+      <Botones alerts={alerts}/>
+		</div>
+	);
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Botones extends React.Component {
+    // props = { alerts: { m1: "Aprobado", m2: "En curso" } }
+	render() {
+		return (
+			<div>
+				<button onClick={() => alert(this.props.alerts.m1)}>Módulo 1</button>
+				<button onClick={() => alert(this.props.alerts.m2)}>Módulo 2</button>
+			</div>
+		);
+	}
+}
+
+// export default Botones;
