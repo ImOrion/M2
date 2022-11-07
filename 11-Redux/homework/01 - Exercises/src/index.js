286f08e8c133653573f62fefd0d82024989cb116
ImOrion <mateou771@gmail.com>
2022-11-07 15:49:23 -0500

    Redux

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,22 +3,51 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.querySelector("#valor");
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
-function renderContador() {
+function renderContador(contador) {
+  const numero = store.getState().contador;
+  valor.innerHTML = numero;
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador();
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
-
+store.subscribe(renderContador);
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+const buttonIncrement = document.querySelector("#incremento");
+const buttonDecrement = document.querySelector("#decremento");
+const buttonIncrementImpar = document.querySelector("#incrementoImpar");
+const buttonIncrementAsync = document.querySelector("#incrementoAsync");
+
+buttonIncrement.addEventListener("click", () => {
+  store.dispatch(incremento());
+});
+
+buttonDecrement.addEventListener("click", () => {
+  store.dispatch(decremento());
+});
+
+buttonIncrementImpar.addEventListener("click", () => {
+  const numero = store.getState().contador;
+  if (!(numero % 2 === 0)) {
+    store.dispatch(incremento());
+  }
+});
+
+buttonIncrementAsync.addEventListener("click", () => {
+  setTimeout(() => {
+    store.dispatch(incremento());
+  }, 1000);
+});
